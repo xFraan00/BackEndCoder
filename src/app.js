060a851f5eb7a4a3e3b5a1385f557522e1272dd5
Francisco Guzman <guzman.franciisco@gmail.com>
2024-05-07 16:05:48 -0300

    Mas correcciones

--- src/app.js
+++ src/app.js
@@ -10,53 +10,50 @@ import { ProductManager } from './Classes/product-functions.js';
 
 const app = express();
 const port = 8080;
-const httpServer = app.listen(port, () => { console.log('Corriendo en el servidor ' + port) });
-const socketServer = new Server(httpServer);
-
-
-const productMngr = new ProductManager('./products.json');
-
+const httpServer = app.listen( port, ()=>{ console.log('Corriendo en el servidor ' + port) } );
+const socketServer = new Server( httpServer );
 app.use(json());
 app.use(urlencoded({ extended: true }));
-
-app.engine('handlebars', handlebars.engine());
-app.set('views', __dirName + '/views');
-app.set('view engine', 'handlebars');
-app.use(express.static(__dirName + '/public'))
-
+app.engine( 'handlebars', handlebars.engine() );
+app.set('views', __dirName + '/views')
+app.set('view engine', 'handlebars')
+app.use(express.static( __dirName + '/public'))
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 app.use('/', viewsRouter)
 
+const newInstance = new ProductManager ()
+
 socketServer.on('connection', socket => {
-    console.log("Nuevo cliente conectado");
-
-    productMngr.getProducts()
-    .then((productos) => {
-        socket.emit('productos', productos);
+    newInstance.getProducts()
+    .then( products => {
+        socket.emit('products', products)
     })
-
-    socket.on('nuevoProducto', producto => {
-        productMngr.addProduct(producto.code, producto.title, producto.description, producto.price, [], producto.stock)
-        .then(() => {
-            productMngr.getProducts()
-            .then((productos) => {
-                socket.emit('productos', productos);
-                socket.emit('respuestaAdd', "Producto agregado");
-            })            
-        })
-        .catch((error) => socket.emit('respuestaAdd', "Error al agregar el producto: " + error.message))
+    socket.on('addProduct', async newProductData => {
+        try {
+            await newInstance.addProduct(
+                newProductData.title,
+                newProductData.description,
+                newProductData.code,
+                newProductData.price,
+                true,
+                newProductData.stock,
+                newProductData.category,
+                newProductData.thumbnail
+            );
+            const products = await newInstance.getProducts();
+            socket.emit('products', products);
+        } catch (error) {
+            console.error('Error al agregar un nuevo producto:', error);
+        }
     });
-    
-    socket.on('eliminarProducto', pid => {
-        productMngr.deleteProduct(pid)
-        .then(() => {
-            productMngr.getProducts()
-            .then((productos) => {
-                socket.emit('productos', productos);
-                socket.emit('respuestaDelete', "Producto eliminado");
-            })            
-        })
-        .catch((error) => socket.emit('respuestaDelete', "Error al eliminar el producto: " + error.message))
+    socket.on('deleteProduct', async productToDelete => {
+        try {
+            await newInstance.deleteProduct(productToDelete)
+            const products = await newInstance.getProducts();
+            socket.emit('products', products);
+        } catch (error) {
+            console.error('Error al eliminar el producto:', error);
+        }
     })
-})
+} )
